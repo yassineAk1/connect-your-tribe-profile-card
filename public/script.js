e4e0f81330ca66c7c14b5020afb4090571acb79b
yassineAk1 <yassine9a6@gmail.com>
2026-02-06 09:43:50 +0100

    minor change

--- public/script.js
+++ public/script.js
@@ -1,17 +1,3 @@
-// const buttons = document.querySelectorAll('button');
-// const backgroundImg = document.querySelector('.background');
-// const profileImg = document.querySelector('.profile');
-
-// profileImg.addEventListener('click', () => {
-//   backgroundImg.src = 'img2.jpg';
-// })
-
-// buttons.forEach((button, index) => {
-//   button.addEventListener('click', () => {
-//     backgroundImg.src = `foto${index + 1}.svg`;
-//   });
-// });
-
 const buttons = document.querySelectorAll("button");
 const backgroundImg = document.querySelector(".background");
 const profileImg = document.querySelector(".profile");
