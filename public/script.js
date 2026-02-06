a4e95e4581b510366cc0c42524ed0553784c7121
yassineAk1 <yassine9a6@gmail.com>
2026-02-06 09:39:54 +0100

    #1 js voor fetch

--- public/script.js
+++ public/script.js
@@ -1,14 +1,41 @@
-const buttons = document.querySelectorAll('button');
-const backgroundImg = document.querySelector('.background');
-const profileImg = document.querySelector('.profile');
+// const buttons = document.querySelectorAll('button');
+// const backgroundImg = document.querySelector('.background');
+// const profileImg = document.querySelector('.profile');
 
+// profileImg.addEventListener('click', () => {
+//   backgroundImg.src = 'img2.jpg';
+// })
 
-profileImg.addEventListener('click', () => {
-  backgroundImg.src = 'img2.jpg';
-})
+// buttons.forEach((button, index) => {
+//   button.addEventListener('click', () => {
+//     backgroundImg.src = `foto${index + 1}.svg`;
+//   });
+// });
 
-buttons.forEach((button, index) => {
-  button.addEventListener('click', () => {
-    backgroundImg.src = `foto${index + 1}.svg`;
+const buttons = document.querySelectorAll("button");
+const backgroundImg = document.querySelector(".background");
+const profileImg = document.querySelector(".profile");
+
+fetch("https://fdnd.directus.app/items/person/278")
+  .then((res) => res.json())
+  .then((result) => {
+    const personData = result.data;
+    personData.custom = JSON.parse(personData.custom);
+
+    const images = [
+      personData.custom.gaming,
+      personData.custom.reizen,
+      personData.custom.sporten,
+      personData.custom.kungfu,
+    ];
+
+    buttons.forEach((button, index) => {
+      button.addEventListener("click", () => {
+        backgroundImg.src = images[index];
+      });
+
+      profileImg.addEventListener("click", () => {
+        backgroundImg.src = personData.custom.background;
+      });
+    });
   });
-});
